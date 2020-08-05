48e968c5e4c7eb7e0e94ccf3f78d7159a17f6686
fabricia-santana <bricia_santana@hotmail.com>
2020-08-04 21:14:43 -0300

    Update BannerMain

--- src/components/BannerMain/components/VideoIframeResponsive/index.js
+++ src/components/BannerMain/components/VideoIframeResponsive/index.js
@@ -4,13 +4,13 @@ import {VideoContainer, ResponsiveIFrame} from './style';
 function VideoIframeResponsive({youtubeID}){
     return(
         <VideoContainer>
-            <ResponsiveIFrame>
+            <ResponsiveIFrame
                 title="Título do Iframe"
                 src={`https:/www.youtube.com/embed/${youtubeID}?autoplay=0&mute=1`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
-            </ResponsiveIFrame>
+            />
         </VideoContainer>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-function CadastroVideo(){
-  return(
-    <div>
-      Página de Cadastro de Video
-    </div>
-  )
-}
+const pagina404 = ()=> (<div>Página 404</div>)
+
 
 ReactDOM.render(
   
-  <BrowserRouter>
-  <Switch>
-    <Route path="/cadastro/video" component={CadastroVideo}/>
-    <Route path="/" component={App}/>
-  </Switch>
-  </BrowserRouter>,  
-  document.getElementById('root')
-);
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo}/>      
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>  
+      <Route component={pagina404}/>
+
+    </Switch>
+    </BrowserRouter>,  
+    document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

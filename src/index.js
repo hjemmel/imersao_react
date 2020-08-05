b72de130b1cdcfb4675bd8e06667da96390cb1cd
fabricia-santana <bricia_santana@hotmail.com>
2020-08-04 21:20:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-const pagina404 = ()=> (<div>Página 404</div>)
-
 
 ReactDOM.render(
   
@@ -16,7 +14,7 @@ ReactDOM.render(
       <Route path="/" component={Home} exact/>
       <Route path="/cadastro/video" component={CadastroVideo}/>      
       <Route path="/cadastro/categoria" component={CadastroCategoria}/>  
-      <Route component={pagina404}/>
+      
 
     </Switch>
     </BrowserRouter>,  

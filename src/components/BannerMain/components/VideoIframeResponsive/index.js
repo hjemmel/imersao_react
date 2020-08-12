a28879e9b49fc43d6a944bbccfd29257e41c1af2
Hans Jakob Emmel <547867+hjemmel@users.noreply.github.com>
2020-08-13 10:46:09 +1200

    Update VideoIframeResponsive to use correct URL

--- src/components/BannerMain/components/VideoIframeResponsive/index.js
+++ src/components/BannerMain/components/VideoIframeResponsive/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';https://github.com/fabricia-santana/imersao_react/tree/master/src/components/BannerMain/components/VideoIframeResponsive
 import {VideoContainer, ResponsiveIFrame} from './style';
 
 function VideoIframeResponsive({youtubeID}){
@@ -6,7 +6,7 @@ function VideoIframeResponsive({youtubeID}){
         <VideoContainer>
             <ResponsiveIFrame
                 title="Título do Iframe"
-                src={`https:/www.youtube.com/embed/${youtubeID}?autoplay=0&mute=1`}
+                src={`https://www.youtube.com/embed/${youtubeID}?autoplay=0&mute=1`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
